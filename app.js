d27e6ca0c1703908b9475bdb6c4b753041e4ed1a
chiragsuri <chiragsuri2016@gmail.com>
2023-04-13 23:06:01 +0530

    Pre deployement changes (14th commit)

--- app.js
+++ app.js
@@ -25,13 +25,17 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+
+const MongoStore = require("connect-mongo");
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 //Setting UP MONGOOSE
 mongoose.set('strictQuery', true);
 
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    await mongoose.connect(dbUrl);
     console.log("DATABASE CONNECTED")
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
@@ -52,11 +56,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize({
     replaceWith: '_'
 }))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
 
 //session function
 const sessionConfig = {
+    store: MongoStore.create({
+        mongoUrl: dbUrl,
+        secret,
+        touchAfter: 24 * 3600 // time period in seconds
+      }),
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
